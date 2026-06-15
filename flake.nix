ecbce5b5d4fd94ec55d1525897f983fecd933fd1
WurzelRitter <109515158+WurzelRitter@users.noreply.github.com>
2026-06-15 09:27:57 +0200

    Define VeloxOS ISO configuration in flake.nix
    Add configuration for VeloxOS ISO generation with ZRAM support.

--- flake.nix
+++ flake.nix
@@ -6,9 +6,31 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    # Hier werden später ISOs und Test-Systeme definiert
+    
     nixosConfigurations = {
-      # Platzhalter für erstes VeloxOS-System
+      velox-direct = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix 
+          ./configuration.nix          
+          ./modules                    
+        ];
+      };
     };
+
+    nixosConfigurations.velox-iso = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix"
+        ./modules
+
+        ({ config, pkgs, ... }: {
+          veloxos.services.zram.enable = true;
+          isoImage.isoName = "veloxos-unstable-${pkgs.stdenv.hostPlatform.system}.iso";
+          nixpkgs.config.allowUnfree = true;
+        })
+      ];
+    };
+
   };
 }
